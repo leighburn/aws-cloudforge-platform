7217937e12ff51120cdf76f699d58717087a72c9
liban <leighburn@outlook.com>
2026-08-05 05:59:02 +0100

    Deploy AWS CloudForge Platform to EC2 and fix Dockerfile

--- app/script.js
+++ app/script.js
@@ -1,4 +1,8 @@
-function showMessage() {
-    document.getElementById("message").innerHTML =
-        "✅ Deployment started successfully!";
-}
+alert("JavaScript Loaded!");
+
+const deployButton = document.getElementById("deployButton");
+const message = document.getElementById("message");
+
+deployButton.addEventListener("click", function () {
+    message.textContent = "✅ Deployment simulation started successfully!";
+});
